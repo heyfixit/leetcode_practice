3dacf75333af625f1573779764be74e0c0afa24c
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-09-03 16:37:55 -0400

    add canJump solution

--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,3 +1,17 @@
-function canJump(nums) {}
+function canJump(nums) {
+  const dpArray = Array(nums.length).fill(false);
+  dpArray[0] = true;
+
+  for (let j = 1; j < nums.length; j++) {
+    for (let i = 0; i < j; i++) {
+      if (dpArray[i] && i + nums[i] >= j) {
+        dpArray[j] = true;
+        break;
+      }
+    }
+  }
+
+  return dpArray[dpArray.length - 1];
+}
 
 module.exports = canJump;
