4bf40e64696b048478775a5820b25379b5d1379d
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-09-03 16:12:10 -0400

    add optimized solution

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,4 +1,4 @@
-function isAnagram(s, t) {
+function isAnagramOld(s, t) {
   // given strings s and t, find if t is an anagram of s
   const [sortedS, sortedT] = [s, t].map(v =>
     v
@@ -8,6 +8,33 @@ function isAnagram(s, t) {
   );
 
   return sortedS === sortedT;
+}
+
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  sCharCounts = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const sChar = s[i];
+    if (sCharCounts[sChar] !== undefined) {
+      sCharCounts[sChar] += 1;
+    } else {
+      sCharCounts[sChar] = 1;
+    }
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    const tChar = t[i];
+    if (sCharCounts[tChar] === undefined || sCharCounts[tChar] === 0) {
+      return false;
+    }
+    sCharCounts[tChar] -= 1;
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
