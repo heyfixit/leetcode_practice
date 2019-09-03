41801c5e1b9a1cd736def6ec7ba0eb148e6c7262
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-09-03 11:31:35 -0400

    add array heavy solution for isAnagram

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,13 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  // given strings s and t, find if t is an anagram of s
+  const [sortedS, sortedT] = [s, t].map(v =>
+    v
+      .split('')
+      .sort()
+      .join('')
+  );
+
+  return sortedS === sortedT;
+}
 
 module.exports = isAnagram;
