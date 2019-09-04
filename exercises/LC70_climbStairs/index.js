273f9c3e54238dc48fd05b2492a53fcf7302e8dd
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-09-04 08:46:43 -0400

    add non recursive solution

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,9 +1,23 @@
-const climbStairs = n => {
+const climbStairs_recursive = n => {
   if (n < 4) {
     return n;
   }
 
   return climbStairs(n - 1) + climbStairs(n - 2);
+};
+
+const climbStairs = n => {
+  if (n < 4) {
+    return n;
+  }
+
+  const answers = [0, 1, 2, 3];
+
+  for (let i = 4; i <= n; i++) {
+    answers.push(answers[i - 1] + answers[i - 2]);
+  }
+
+  return answers[answers.length - 1];
 };
 
 module.exports = climbStairs;
