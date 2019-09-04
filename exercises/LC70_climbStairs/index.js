96f355ef882e602398a5f0b89051eafbc1e0dbba
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-09-04 08:29:22 -0400

    add climbStairs solution

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,3 +1,9 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+  if (n < 4) {
+    return n;
+  }
+
+  return climbStairs(n - 1) + climbStairs(n - 2);
+};
 
 module.exports = climbStairs;
